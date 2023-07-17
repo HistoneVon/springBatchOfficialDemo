5698e513aa4ecc9dae9b3aec973398fc3d5e52d1
HistoneVon <fengclchn@outlook.com>
2023-07-17 10:03:36 +0800

    initial commit

---
+++ src/main/java/com/example/springbatchofficialdemo/SpringBatchOfficialDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbatchofficialdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author histonevon
+ */
+@SpringBootApplication
+public class SpringBatchOfficialDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchOfficialDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatchofficialdemo/SpringBatchOfficialDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchofficialdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchOfficialDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
