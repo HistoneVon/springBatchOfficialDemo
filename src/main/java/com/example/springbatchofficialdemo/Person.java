77e40a0d1f12e36b657798721758473960eda377
HistoneVon <fengclchn@outlook.com>
2023-07-25 15:14:05 +0800

    update lombok and readme

--- src/main/java/com/example/springbatchofficialdemo/Person.java
+++ src/main/java/com/example/springbatchofficialdemo/Person.java
@@ -1,11 +1,14 @@
 package com.example.springbatchofficialdemo;
 
+import lombok.Data;
+
 /**
  * @author Histone Von fengclchn@outlook.com
  * @createdBy histonevon
  * @date 2023/7/17 10:16
  */
 
+@Data
 public class Person {
 
     private String lastName;
@@ -16,22 +19,6 @@ public class Person {
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
